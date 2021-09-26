5ad76fb78324d93f96f4f2f6e9c15f02e68e5e34
levintoo <tooklevin@gmail.com>
2021-09-26 21:28:58 +0300

    display admin products

--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -1,3 +1,41 @@
 <div>
-     If you look to others for fulfillment, you will never truly be fulfilled.
+     <div class="container" style="padding: 30px 0 ;">
+         <div class="row">
+             <div class="col-md-12">
+                 <div class="panel panel-default">
+                     <div class="panel-heading">
+                         All Products
+                     </div>
+                     <div class="panel-body">
+                         <table class="table table-striped">
+                             <thead>
+                             <tr>
+                                 <th>Id</th>
+                                 <th>Image</th>
+                                 <th>Stock</th>
+                                 <th>Price</th>
+                                 <th>Category</th>
+                                 <th>Date</th>
+                                 <th>Action</th>
+                             </tr>
+                             </thead>
+                             <tbody>
+                             @foreach($products as $product)
+                                 <tr>
+                                     <td>{{$product->id}}</td>
+                                     <td><img src="{{asset('assets/images/products')}}/{{$product->image}}" width="60" /></td>
+                                     <td>{{$product->stock_status}}</td>
+                                     <td>{{$product->regular_price}}</td>
+                                     <td>{{$product->category_id}}</td>
+                                     <td>{{$product->created_at}}</td>
+                                     <td></td>
+                                 </tr>
+                             @endforeach
+                             </tbody>
+                         </table>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
 </div>
