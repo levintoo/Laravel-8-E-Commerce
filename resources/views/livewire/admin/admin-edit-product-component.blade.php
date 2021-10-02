36dc6057180e32c0483094957bc5e3359bb39b98
levintoo <tooklevin@gmail.com>
2021-10-02 12:42:58 +0300

    admin edit product

---
+++ resources/views/livewire/admin/admin-edit-product-component.blade.php
@@ -0,0 +1,130 @@
+<div>
+    <style>
+        nav svg{
+            height: 20px;
+        }
+        nav .hidden{
+            display: block !important;
+        }
+    </style>
+    <div class="container" style="padding: 30px 0 ;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                Edit Product
+                            </div>
+                            <div class="col-md-6">
+                                <a href="{{route('admin.products')}}" class="btn btn-success pull-right" >All Products</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        @if(Session::has('message'))
+                            <dv class="alert alert-success" role="alert">{{Session::get('message')}}</dv>
+                        @endif
+
+                        <form class="form-horizontal" method="POST" enctype="multipart/form-data"  wire:submit.prevent="updateProduct">
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Product Name</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="Product Name" class="form-control input-md" wire:model="name" wire:keyup="generateslug">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Product Slug</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="Category Slug" class="form-control input-md"  wire:model="slug" >
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Short Description</label>
+                                <div class="col-md-4">
+                                    <textarea class="form-control  input-md" wire:model="short_description"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Description</label>
+                                <div class="col-md-4">
+                                    <textarea class="form-control input-md" rows="4" wire:model="description"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Regular Price</label>
+                                <div class="col-md-4">
+                                    <input type="number" placeholder="Regular Price" class="form-control input-md" wire:model="regular_price">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Sale Price</label>
+                                <div class="col-md-4">
+                                    <input type="number" placeholder="Sale Price" class="form-control input-md" wire:model="sale_price">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">SKU</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="SKU" class="form-control input-md" wire:model="SKU">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Stock</label>
+                                <div class="col-md-4">
+                                    <select class="form-control input-md" wire:model="stock_status">
+                                        <option value="instock">InStock</option>
+                                        <option value="ouofstock">Out of Stock</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Featured</label>
+                                <div class="col-md-4">
+                                    <select class="form-control input-md" wire:model="featured">
+                                        <option value="0">No</option>
+                                        <option value="1">Yes</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Quantity</label>
+                                <div class="col-md-4">
+                                    <input type="number" placeholder="Quantity" class="form-control input-md" wire:model="quantity">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Product Image</label>
+                                <div class="col-md-4">
+                                    <input type="file" class="input-file" wire:model="newimage" >
+                                    @if($newimage)
+                                        <img src="{{$newimage->temporaryUrl()}}" width="120">
+                                    @else
+                                        <img src="{{asset('assets/images/products')}}/{{$image}}" width="120">
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Category</label>
+                                <div class="col-md-4">
+                                    <select class="form-control input-md" wire:model="category_id" required>
+                                        <option >Select category</option>
+                                        @foreach($categories as $category)
+                                            <option value={{$category->id}}>{{$category->name}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label"></label>
+                                <div class="col-md-4">
+                                    <input href="" class="btn btn-primary" type="submit" value="Update">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
