543230a5d84b12502f33a8a3ba0cd8d8c575fefe
levintoo <tooklevin@gmail.com>
2021-10-02 16:27:16 +0300

    home slider

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminAddHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminAddProductComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
+use App\Http\Livewire\Admin\AdminEditHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminEditProductComponent;
+use App\Http\Livewire\Admin\AdminHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
@@ -60,4 +63,7 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function (){
     Route::get('admin/addproducts',AdminAddProductComponent::class)->name('admin.addproducts');
     Route::get('admin/product/edit/{product_slug}',AdminEditProductComponent::class)->name('admin.editproduct');
 
+    Route::get('admin/slider', AdminHomeSliderComponent::class)->name('admin.homeslider');
+    Route::get('admin/slider/add', AdminAddHomeSliderComponent::class)->name('admin.addhomeslider');
+    Route::get('admin/slider/edit/{slide_id}', AdminEditHomeSliderComponent::class)->name('admin.edithomeslider');
 });
