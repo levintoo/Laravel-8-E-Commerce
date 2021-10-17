7929ec9cc07e13c939edb7f539864418cf65d34a
levintoo <tooklevin@gmail.com>
2021-10-17 21:00:00 +0300

    home categories dinamic

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -16,7 +16,7 @@ class HomeComponent extends Component
         $lproducts = Product::orderBy('created_at','DESC')->get()->take(8);
         $category = HomeCategory::find(1);
         $cats = explode(',',$category->sel_categories);
-        $categories = Category::whereIn('id',$cats);
+        $categories = Category::whereIn('id',$cats)->get();
         $no_of_products = $category->no_of_products;
         return view('livewire.home-component',['sliders'=>$sliders,'lproducts'=>$lproducts,'categories'=>$categories,'no_of_products'=>$no_of_products])->layout('layouts.base');
     }
