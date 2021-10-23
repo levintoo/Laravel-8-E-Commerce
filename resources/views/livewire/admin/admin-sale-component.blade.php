10d6230f152626ea3fdbcb086b3ecf67002669b1
levintoo <tooklevin@gmail.com>
2021-10-23 22:01:42 +0300

    datetimepicker

---
+++ resources/views/livewire/admin/admin-sale-component.blade.php
@@ -0,0 +1,65 @@
+<div>
+    <style>
+        nav svg{
+            height: 20px;
+        }
+        nav .hidden{
+            display: block !important;
+        }
+    </style>
+    <div class="container" style="padding: 30px 0 ;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                Sale setting
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        @if(Session::has('message'))
+                            <dv class="alert alert-success" role="alert">{{Session::get('message')}}</dv>
+                        @endif
+                    <form class="form-horizontal">
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Status</label>
+                            <div class="col-md-4">
+                                <select class="form-control">
+                                    <option value="0">Inactive</option>
+                                    <option value="1">Active</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Sale date</label>
+                            <div class="col-md-4">
+                                <input type="text" id="sale-date" placeholder="YYYY/MM/DD H:M:S" class="form-control input-md">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label"></label>
+                            <div class="col-md-4">
+                                <input type="submit" class="btn btn-primary" value="update">
+                            </div>
+                        </div>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@push('scripts')
+    <script>
+        $(function(){
+            $('#sale-date').datetimepicker({
+                format : 'Y-MM-DD h:m:s',
+            })
+            .on('dp.change',function(ev){
+
+            });
+        });
+    </script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Livewire\Admin\AdminEditProductComponent;
 use App\Http\Livewire\Admin\AdminHomeCategoryComponent;
 use App\Http\Livewire\Admin\AdminHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
+use App\Http\Livewire\Admin\AdminSaleComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\CartComponent;
@@ -68,4 +69,5 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function (){
     Route::get('admin/slider/add', AdminAddHomeSliderComponent::class)->name('admin.addhomeslider');
     Route::get('admin/slider/edit/{slide_id}', AdminEditHomeSliderComponent::class)->name('admin.edithomeslider');
     Route::get('admin/home-categories',AdminHomeCategoryComponent::class)->name('admin.homecategories');
+    Route::get('admin/sale',AdminSaleComponent::class)->name('admin.sale');
 });
