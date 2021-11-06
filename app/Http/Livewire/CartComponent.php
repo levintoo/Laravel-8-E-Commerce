46d6107d31c6958ac82ef53bf23f6451353de174
levintoo <tooklevin@gmail.com>
2021-11-06 15:07:50 +0300

    adding products to wishlist

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -10,24 +10,24 @@ class CartComponent extends Component
 {
     public function increaseQuantity($rowId)
     {
-        $product = Cart::get($rowId);
+        $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty + 1;
-        Cart::update($rowId,$qty);
+        Cart::instance('cart')->update($rowId,$qty);
     }
     public function decreaseQuantity($rowId)
     {
-        $product = Cart::get($rowId);
+        $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty - 1;
-        Cart::update($rowId,$qty);
+        Cart::instance('cart')->update($rowId,$qty);
     }
     public function destroy($rowId)
     {
-        Cart::remove($rowId);
+        Cart::instance('cart')->remove($rowId);
         return redirect()->back()->with('success_message', 'Product Removed from cart');
     }
     public function destroyAll()
     {
-        Cart::destroy();
+        Cart::instance('cart')->destroy();
         session()->flush('success_message','Cart cleared');
     }
     public function render()
